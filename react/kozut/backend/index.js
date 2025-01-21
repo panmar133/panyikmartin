b47e24ba714e4a9f90b971ccfc54cd06aa5c1aec
MartinOfficial <47602439+MartinOfficial@users.noreply.github.com>
2025-01-21 12:24:12 +0100

    2025.01.21 órai
    nodejs telepítés

---
+++ react/kozut/backend/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = require();
+const cors = require("cors");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(cors());
