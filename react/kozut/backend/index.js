aeb16d2116c7717fe6e9b9cbc3591eb0d868930a
MartinOfficial <47602439+MartinOfficial@users.noreply.github.com>
2025-01-24 14:19:00 +0100

    2025.01.24. órai
    MySQL csatlakozás + 1-2 GET.

--- react/kozut/backend/index.js
+++ react/kozut/backend/index.js
@@ -1,7 +1,41 @@
 const express = require("express");
-const app = require();
+const app = express();
 const cors = require("cors");
 const mysql = require("mysql");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(cors());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "127.0.0.1",
+    port: 3307,
+    password: "",
+    database: "kozutak"
+});
+
+app.get("/", (req, res) => {
+    res.send("Backend mükszik.");
+});
+
+app.get("/regiok:id", (req, res) => {
+    const sql = "SELECT * FROM regiok WHERE regiok.Rid = ?;";
+    db.query(sql, id, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    });
+});
+
+app.get("/regiokwhere", (req, res) => {
+    const sql = "SELECT * FROM regiok WHERE regiok.Rid = 8;";
+    db.query(sql, (err, result) => {
+        if (err) {
+            return res.json(err);
+        }
+        return res.json(result);
+    });
+});
+
+app.listen(3001, () => {
+    console.log("A szerver elindult a 3001-es porton.");
+});
