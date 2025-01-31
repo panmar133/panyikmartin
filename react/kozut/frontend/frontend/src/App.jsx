6baa2a5b495be6a9f0fdd0f4b551256245fc976f
MartinOfficial <47602439+MartinOfficial@users.noreply.github.com>
2025-01-31 14:19:07 +0100

    2025.01.31. órai
    Vite + React telepítés (Közút)

---
+++ react/kozut/frontend/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      
+    </div>
+      <h1>Közutak</h1>
+     
+    </>
+  )
+}
+
+export default App
