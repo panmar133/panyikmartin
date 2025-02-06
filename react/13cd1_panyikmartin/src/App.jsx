9049d14e4d491987faf1b6e6a03a4753649a1e7a
MartinOfficial <47602439+MartinOfficial@users.noreply.github.com>
2025-02-06 13:09:05 +0100

    2025.02.06 órai

---
+++ react/13cd1_panyikmartin/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='container'>TZ</h1>
+      <div>
+        <p>Ez a főoldal.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
